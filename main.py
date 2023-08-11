fe2bee4459a4f4095bfb24fddc7a61c87227fb59
Georgie Santiago <georgiesantiago@Georgies-MacBook-Pro.local>
2023-08-10 22:10:32 -0400

    initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+'''
+Write code that reads the keys and values from the text file data.txt
+
+Expected Output
+"key: <KEY> value: <VALUE>"
+
+
+Main google search:
+How to get keys and values from a textfile python
+
+You should find out
+1. How to read the text from a file
+2. How to split a string: split by the "="
+3. Print out the results
+
+'''
+
+def print_file()
+    #Delete pass and start here
+    pass
